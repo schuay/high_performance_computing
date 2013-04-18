cf94436d05a404de54a0d41a8b3d8e31f0b07033
Jakob Gruber <jakob.gruber@gmail.com>
2013-04-18 15:24:43 +0200

    An iterator interface to itree_t

---
+++ block1-lru/itree.c
@@ -0,0 +1,8 @@
+#include "itree.h"
+
+struct __itree_iter_t {
+    const itree_t *root;        /**< The root of the iterated tree. */
+    const itree_t *stack[32];   /**< The node stack. Tree depth cannot exceed 32
+                                     since keys are uint32_t. */
+    int top;                    /**< The current top stack index. */
+};
--- block1-lru/itree.h
+++ block1-lru/itree.h
@@ -1,16 +1,18 @@
 #ifndef __ITREE_H
 #define __ITREE_H
 
+#include <stdint.h>
+
 /**
  * An AVL tree with closed, mutually disjunct uint32_t intervals as keys and
  * uint32_t values representing the index count in the right subtree (the
  * interval [5, 7] counts as 3 indices).
  */
 
-typedef struct {
-    itree_t *l, *r;         /**< The left and right child nodes. */
-    uint32_t k1, k2;        /**< The key interval [k1, k2]. */
-    uint32_t v;             /**< The # of elements in the right subtree. */
+typedef struct __itree_t {
+    struct __itree_t *l, *r;    /**< The left and right child nodes. */
+    uint32_t k1, k2;            /**< The key interval [k1, k2]. */
+    uint32_t v;                 /**< The # of elements in the right subtree. */
 } itree_t;
 
 /**
@@ -20,5 +22,15 @@ typedef struct {
 uint32_t
 itree_insert(const uint32_t index,
              itree_t **root);
+
+struct __itree_iter_t;
+typedef struct __itree_iter_t itree_iter_t;
+
+void
+itree_iter_init(itree_iter_t *iter,
+                const itree_t *root);
+
+const itree_t *
+itree_iter_next(itree_iter_t *iter);
 
 #endif /*  __ITREE_H */
