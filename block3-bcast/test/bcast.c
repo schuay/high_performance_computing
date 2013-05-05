37d3ae15ad73593a57b74e22445c19eb697a03af
Jakob Gruber <jakob.gruber@gmail.com>
2013-05-05 17:39:46 +0200

    Add a test macro

--- block3-bcast/test/bcast.c
+++ block3-bcast/test/bcast.c
@@ -6,36 +6,35 @@
 #include "native.h"
 #include "util.h"
 
-#define ROOT (0)
-#define SEED (0)
-#define N (42)
-
-START_TEST(bcast_1)
-{
-    int rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-    int *data;
-
-    if (rank == ROOT) {
-        data = array_random(SEED, N);
-        fail_unless(data != NULL);
-    } else {
-        data = malloc(N * sizeof(int));
-        fail_unless(data != NULL);
-    }
-
-    fail_unless(bcast_native(data, N, ROOT, MPI_COMM_WORLD) == MPI_SUCCESS); 
-
-    int *expected = array_random(SEED, N);
-    fail_unless(expected != NULL);
-
-    fail_unless(memcmp(expected, data, N * sizeof(int)) == 0);
-
-    free(data);
-    free(expected);
-}
+#define BCAST_TEST(name, fn, comm, seed, n, root) \
+START_TEST(name) \
+{ \
+    int rank; \
+    MPI_Comm_rank(comm, &rank); \
+ \
+    int *data; \
+ \
+    if (rank == root) { \
+        data = array_random(seed, n); \
+        fail_unless(data != NULL); \
+    } else { \
+        data = malloc(n * sizeof(int)); \
+        fail_unless(data != NULL); \
+    } \
+ \
+    fail_unless(fn(data, n, root, comm) == MPI_SUCCESS);  \
+ \
+    int *expected = array_random(seed, n); \
+    fail_unless(expected != NULL); \
+ \
+    fail_unless(memcmp(expected, data, n * sizeof(int)) == 0); \
+ \
+    free(data); \
+    free(expected); \
+} \
 END_TEST
+
+BCAST_TEST(native01, bcast_native, MPI_COMM_WORLD, 0, 42, 0)
 
 static Suite *
 create_suite(void)
@@ -43,7 +42,7 @@ create_suite(void)
     Suite *s = suite_create(__FILE__);
     TCase *tc_core = tcase_create("core");
 
-    tcase_add_test(tc_core, bcast_1);
+    tcase_add_test(tc_core, native01);
 
     suite_add_tcase(s, tc_core);
 
