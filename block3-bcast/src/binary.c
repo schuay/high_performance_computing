cd09a4e99b82da20c10701970da1658a5b775427
Jakob Gruber <jakob.gruber@gmail.com>
2013-05-05 21:05:24 +0200

    Implement the pipelined binary tree

--- block3-bcast/src/binary.c
+++ block3-bcast/src/binary.c
@@ -1,10 +1,104 @@
 #include "binary.h"
 
+#include "util.h"
+
+#define BLOCK_SIZE (1024)
+
 int
 bcast_binary(int *buffer,
              int count,
              int root,
              MPI_Comm comm)
 {
-    return -1;
+    int ret;
+    
+    int size, rank;
+    MPI_Comm_size(comm, &size);
+    MPI_Comm_rank(comm, &rank);
+
+    /* 0-based rank for easier tree calculations. */
+    const int rank0 = (rank + size - root) % size;
+
+    const int parent0 = (rank0 - 1) / 2;
+    const int l0 = rank0 * 2 + 1;
+    const int r0 = rank0 * 2 + 2;
+
+    const int has_l = (l0 < size);
+    const int has_r = (r0 < size);
+
+    const int parent = (root + parent0) % size;
+    const int l = (root + l0) % size;
+    const int r = (root + r0) % size;
+
+    if (rank == root) {
+        for (int i = 0; i < count; i += BLOCK_SIZE) {
+            const int n = MIN(BLOCK_SIZE, count - i);
+
+            if (has_l) {
+                ret = MPI_Send(buffer + i, n, MPI_INT, l, i, comm);
+                if (ret != MPI_SUCCESS) {
+                    return -1;
+                }
+            }
+
+            if (has_r) {
+                ret = MPI_Send(buffer + i, n, MPI_INT, r, i, comm);
+                if (ret != MPI_SUCCESS) {
+                    return -1;
+                }
+            }
+        }
+    } else {
+        int n = MIN(BLOCK_SIZE, count);
+
+        ret = MPI_Recv(buffer, n, MPI_INT, parent, 0, comm, MPI_STATUS_IGNORE);
+        if (ret != MPI_SUCCESS) {
+            return -1;
+        }
+
+        int last_i = 0, last_n = n;
+        for (int i = BLOCK_SIZE; i < count; i += BLOCK_SIZE) {
+            n = MIN(BLOCK_SIZE, count - i);
+
+            if (has_l) {
+                ret = MPI_Send(buffer + last_i, last_n, MPI_INT, l, last_i, comm);
+                if (ret != MPI_SUCCESS) {
+                    return -1;
+                }
+            }
+
+            if (has_r) {
+                ret = MPI_Send(buffer + last_i, last_n, MPI_INT, r, last_i, comm);
+                if (ret != MPI_SUCCESS) {
+                    return -1;
+                }
+            }
+
+            /* TODO: Async receive. */
+
+            ret = MPI_Recv(buffer + i, n, MPI_INT, parent, i, comm, MPI_STATUS_IGNORE);
+            if (ret != MPI_SUCCESS) {
+                return -1;
+            }
+
+            last_i = i;
+            last_n = n;
+        }
+
+        if (has_l) {
+            ret = MPI_Send(buffer + last_i, last_n, MPI_INT, l, last_i, comm);
+            if (ret != MPI_SUCCESS) {
+                return -1;
+            }
+        }
+
+        if (has_r) {
+            ret = MPI_Send(buffer + last_i, last_n, MPI_INT, r, last_i, comm);
+            if (ret != MPI_SUCCESS) {
+                return -1;
+            }
+        }
+    }
+
+    return 0;
 }
