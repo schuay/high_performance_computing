aa28a7ff304b3f8c730abf408e7be436d498e4c3
Jakob Gruber <jakob.gruber@gmail.com>
2013-07-08 12:49:15 +0200

    Add a test case comparing naive against the lru algorithm
    This test exposes a mistake in the itree implementation.

--- block1-lru/test/stackdist.c
+++ block1-lru/test/stackdist.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "naive.h"
 #include "stackdist.h"
 
 #define SI (STACKDIST_INFINITE)
@@ -25,6 +26,68 @@ START_TEST(test_1)
 }
 END_TEST
 
+static uint64_t *t;
+static uint64_t *naive;
+static uint64_t *lru;
+
+static void
+lru_callback(uint64_t index, uint64_t distance)
+{
+    lru[index] = distance;
+}
+
+static void
+naive_callback(uint64_t index, uint64_t distance)
+{
+    naive[index] = distance;
+}
+
+static int
+tracecmp(const uint64_t *naive,
+         const uint64_t *lru,
+         const uint64_t *trace,
+         const uint64_t n)
+{
+    int not_equal = 0;
+    for (uint64_t i = 0; i < n; i++) {
+        if (naive[i] == lru[i]) {
+            continue;
+        }
+        not_equal = 1;
+        fprintf(stderr, "i: %llu, t[i]: %llu, naive: %llu, lru: %llu\n",
+                (long long unsigned)i,
+                (long long unsigned)trace[i],
+                (long long unsigned)naive[i],
+                (long long unsigned)lru[i]);
+    }
+    return not_equal;
+}
+
+START_TEST(test_2)
+{
+    const uint64_t n = 128;
+    const uint64_t domain = 32;
+
+    t = malloc(n * sizeof(uint64_t));
+    naive = malloc(n * sizeof(uint64_t));
+    lru = malloc(n * sizeof(uint64_t));
+
+    srand(10);
+    for (uint64_t i = 0; i < n; i++) {
+        t[i] = rand() % domain;
+    }
+
+    fail_unless(naive_process_trace(t, n, naive_callback) == 0);
+    fail_unless(stackdist_process_trace(t, n, lru_callback) == 0);
+
+    fail_unless(tracecmp(naive, lru, t, n) == 0);
+
+    free(t);
+    free(naive);
+    free(lru);
+}
+END_TEST
+
 static Suite *
 create_suite(void)
 {
@@ -32,6 +95,7 @@ create_suite(void)
     TCase *tc_core = tcase_create("core");
 
     tcase_add_test(tc_core, test_1);
+    tcase_add_test(tc_core, test_2);
 
     suite_add_tcase(s, tc_core);
 
